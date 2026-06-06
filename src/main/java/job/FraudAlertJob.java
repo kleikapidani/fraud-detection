652e00336eab4672fc44e844e8dde50d30a3b787
Klei Kapidani <kleikapidani24@gmail.com>
2026-06-06 18:19:56 +0200

    added println

--- src/main/java/job/FraudAlertJob.java
+++ src/main/java/job/FraudAlertJob.java
@@ -4,5 +4,6 @@ public class FraudAlertJob {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Test");
+        System.out.println("Test");
     }
 }
